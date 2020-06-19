4f78b5a0aa6e1cd461db44bac337eb84ad59a55a
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-19 23:53:35 +0530

    changes in contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 var SocialNetwork = artifacts.require('./SocialNetwork.sol')
 
+// deploying contract
+
 module.exports = function (deployer) {
   deployer.deploy(SocialNetwork)
 }

2873c72beac638da7071716638dc65ac570954a3
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-20 22:50:21 +0530

    added post struct and event

--- test/SocialNetwork.test.js
+++ test/SocialNetwork.test.js
@@ -3,7 +3,7 @@ const SocialNetwork = artifacts.require('./SocialNetwork.sol')
 
 require('chai').use(require('chai-as-promised')).should()
 
-contract('SocialNetwork', (accounts) => {
+contract('SocialNetwork', ([deployer, author, tipper]) => {
   let socialNetwork
 
   before(async () => {
@@ -24,4 +24,29 @@ contract('SocialNetwork', (accounts) => {
       assert.equal(name, 'Social Network')
     })
   })
+  describe('posts', async () => {
+    let result, postCount
+
+    before(async () => {
+      result = await socialNetwork.createPost('This is my first post', { from: author })
+      postCount = await socialNetwork.postCount()
+    })
+
+    it('creates posts', async () => {
+      // SUCESS
+      assert.equal(postCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(event.content, 'This is my first post', 'content is correct')
+      assert.equal(event.tipAmount, '0', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      // FAILURE: Post must have content
+      await socialNetwork.createPost('', { from: author }).should.be.rejected;
+    })
+
+  })
 })
+
+})
+
